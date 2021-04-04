6d9287d6ce7f82ffa04043193dea51b3d74ef0de
Haoyin Xu <haoyinxu@gmail.com>
2021-04-04 18:21:56 -0400

    Revert "Revert "Add files via upload""
    This reverts commit 26d3027518700f3b6303543fe8655928ffa8cb7f.

--- pembrolizumab_eqns.m
+++ pembrolizumab_eqns.m
@@ -1,42 +1,15 @@
 function dydt = pembrolizumab_eqns(t, y, p)
 
-% Define normal distributions
-nd1 = makedist('Normal', 0, sqrt(0.134));
-nd2 = makedist('Normal', 0, sqrt(0.0417));
-
-% Calculate partial pre-parameters
-cl = 0.202 * (p.weight / 76.8)^0.595 * (p.BSLD / 89.6)^0.0872 ...
-    * (p.eGFR / 88.47)^0.135 * (p.ALB / 39.6)^-0.907 ...
-    * exp(random(nd1));% L
-v1 = 3.48 * (p.weight / 76.8)^0.489 * (p.ALB / 39.6)^-2.08 ...
-    * exp(random(nd2));% L
-v2 = 4.06 * (p.weight / 76.8)^0.489 * exp(random(nd2)); % L
-q = 0.795 * (p.weight / 76.8)^0.595 * exp(random(nd1)); % L/Day
-
-% Complete pre-parameters by conditions
-if p.sex == 2
-    cl = cl * (1 - 0.152);
-    v1 = v1 * (1 - 0.134);
-end
-
-if p.NSCLC == 1
-    cl = cl * (1 + 0.145);
-end
-
-if p.ECOGPS == 0
-    cl = cl * (1 - 0.0739);
-end
-
-if p.IPI == 1
-    cl = cl * (1 + 0.140);
-    v1 = v1 * (1 + 0.0737);
-end
+% Pre-parameters
+cl = p.cl;
+v1 = p.v1;
+v2 = p.v2;
+Q = p.Q;
 
 % Calculate parameters
-kcl = cl / v1; % 1/Day
-k12 = q / v1; % 1/Day
-k21 = q / v2; % 1/Day
-
+kcl = cl / v1; % 1/day
+k12 = Q / v1; % 1/day
+k21 = Q / v2; % 1/day
 
 dydt = zeros(3, 1);
 
